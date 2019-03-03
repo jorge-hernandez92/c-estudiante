74eb9194f7757521a9d39ffbcc47856f91840f3f
GabyhdzM <48144007+GabyhdzM@users.noreply.github.com>
2019-03-03 15:20:16 -0600

    nueva estructura

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_CALIFICACIONES 5
+
+void limpia(){
+    while ((getchar()) != '\n' ) { }
+}
+
+
+struct Estudiante {
+    char  nombre[50];
+    int   edad;
+    char  genero[20];
+    int   calificaciones[MAX_CALIFICACIONES];
+    int   promedio;
+};
+
+
+int main( ) {
+
+
+    int numeroEstudiante;
+    printf("Ingresa el numero de estudiantes");
+    printf("\n");
+    scanf("%i",&numeroEstudiante);
+    printf( "Numero de estudiantes : %d\n", numeroEstudiante);
+
+    limpia();
+
+    struct Estudiante estudianteArreglo[numeroEstudiante];
+
+    for (int j = 0; j < numeroEstudiante ; j++) {
+        int suma=0;
+        printf("Ingresa el nombre del estudiante\n");
+        fgets(estudianteArreglo[j].nombre, 50, stdin);
+        printf("Ingresa la edad de %s\n", estudianteArreglo[j].nombre);
+        scanf("%i",&estudianteArreglo[j].edad);
+        limpia();
+        printf("Ingresa el genero de  %s\n",estudianteArreglo[j].nombre);
+        fgets(estudianteArreglo[j].genero, 50, stdin);
+        for (int i = 0; i < MAX_CALIFICACIONES; i++) {
+            printf("Ingresa la calificacion %i de %s\n", i, estudianteArreglo[j].nombre);
+            scanf("%i", &estudianteArreglo[j].calificaciones[i]);
+            limpia();
+            suma = suma + estudianteArreglo[j].calificaciones[i];
+        }
+        estudianteArreglo[j].promedio = suma / MAX_CALIFICACIONES;
+    }
+
+
+    for (int j = 0; j < numeroEstudiante ; j++) {
+        printf("\n");
+        printf( "nombre  : %s\n", estudianteArreglo[j].nombre);
+        printf( "genero  : %s\n", estudianteArreglo[j].genero);
+        printf( "edad  : %i\n", estudianteArreglo[j].edad);
+        for (int i = 0; i < MAX_CALIFICACIONES ; i++) {
+            printf("calificacion : %i \n", estudianteArreglo[j].calificaciones[i]);
+        }
+        printf("PROMEDIO : %i\n", estudianteArreglo[j].promedio);
+    }
+
+
+    return 0;
+}
