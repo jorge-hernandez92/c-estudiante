976d857aff00d3f5d47cadbead19ee8563b346fb
Jorge Antonio Hernandez Morales <jah007e@email.wal-mart.com>
2019-03-03 11:07:28 -0600

    Agrege Calificaciones

--- estudiante.c
+++ estudiante.c
@@ -24,7 +24,7 @@ int main( ) {
     printf("Ingresa el numero de estudiantes");
     printf("\n");
     scanf("%i",&numeroEstudiante);
-    printf( "Numero de esrudiantes : %d\n", numeroEstudiante);
+    printf( "Numero de estudiantes : %d\n", numeroEstudiante);
 
     limpia();
 
@@ -33,18 +33,29 @@ int main( ) {
     for (int j = 0; j < numeroEstudiante ; j++) {
         printf("Ingresa el nombre del estudiante\n");
         fgets(estudianteArreglo[j].nombre, 50, stdin);
-        printf("Ingresa la edad del estudiante\n");
+        printf("Ingresa la edad de %s\n", estudianteArreglo[j].nombre);
         scanf("%i",&estudianteArreglo[j].edad);
         limpia();
-        printf("Ingresa el genero del estudiante\n");
+        printf("Ingresa el genero de  %s\n",estudianteArreglo[j].nombre);
         fgets(estudianteArreglo[j].genero, 50, stdin);
+        for (int i = 0; i < MAX_CALIFICACIONES; ++i) {
+            printf("Ingresa la calificacion %i de %s\n",i, estudianteArreglo[j].nombre);
+            scanf("%i",&estudianteArreglo[j].calificaciones[i]);
+            limpia();
+        }
     }
+
 
     for (int j = 0; j < numeroEstudiante ; j++) {
         printf("\n");
         printf( "nombre  : %s\n", estudianteArreglo[j].nombre);
         printf( "genero  : %s\n", estudianteArreglo[j].genero);
-        printf( "genero  : %i\n", estudianteArreglo[j].edad);
+        printf( "edad  : %i\n", estudianteArreglo[j].edad);
+        for (int i = 0; i < MAX_CALIFICACIONES ; ++i) {
+            printf("calificacion : %i \n", estudianteArreglo[j].calificaciones[i]);
+
+        }
+
     }
 
 
